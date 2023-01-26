9bdadbd2e73e626cd8b60ca7780ce556c461bf53
Sagar Mittal <mittalsagar006@gmail.com>
2023-01-26 22:04:47 +0530

    showing Error Page using errorElement

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import RootLayout from './pages/Root';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
-import RootLayout from './pages/Root';
+import ErrorPage from './pages/Error';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: '/', element: <HomePage /> },
       { path: '/products', element: <ProductsPage /> },
