8370c1e5114bb9e77c3d0b1dd9166a81f6b5f17c
Sagar Mittal <mittalsagar006@gmail.com>
2023-01-27 00:35:37 +0530

    defining & using dynamic routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import RootLayout from './pages/Root';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 import ErrorPage from './pages/Error';
+import ProductDetailPage from './pages/ProductDetail';
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,7 @@ const router = createBrowserRouter([
     children: [
       { path: '/', element: <HomePage /> },
       { path: '/products', element: <ProductsPage /> },
+      { path: '/products/:productId', element: <ProductDetailPage /> },
     ],
   },
 ]);
---
+++ src/pages/ProductDetail.js
@@ -0,0 +1,12 @@
+import { useParams } from "react-router-dom";
+
+const ProductDetailPage = () => {
+  const params = useParams();
+
+  return <>
+    <h1>Products Details</h1>
+    <p>{params.productId}</p>
+  </>
+};
+
+export default ProductDetailPage;
